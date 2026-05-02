a120e3a3fca412673d8b0e3f563244240984efe2
Shivaramakrishnaganji <ganjishivaramakrishna@gmail.com>
2026-05-02 15:26:30 +0530

    add project files

--- middleware.js
+++ middleware.js
@@ -41,12 +41,8 @@ module.exports.isOwner= async(req,res,next)=>{
 module.exports.isReviewAuthor= async(req,res,next)=>{
          let {reviewId,id}=req.params;
         let review=await Review.findById(reviewId);
-        if(!review){
-            req.flash('error',"Review does not exist");
-            return res.redirect(`/listings/${id}`);
-        }
         if(!review.author.equals(res.locals.currUser._id)){
-            req.flash('error',"You are not the owner of review");
+            req.flash('error',"You are not the author of review");
             return res.redirect(`/listings/${id}`);
         }
         next();
